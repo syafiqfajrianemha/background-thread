35713b7fcf8d6025dac98f5ed7bc9b9ae6737926
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-08-26 16:33:28 +0700

    first commit

---
+++ app/src/main/java/com/emha/mybackgroundthread/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.emha.mybackgroundthread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val tvStatus = findViewById<TextView>(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default) {
+                // simulate process in background thread
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+
+                    withContext(Dispatchers.Main) {
+                        // update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
